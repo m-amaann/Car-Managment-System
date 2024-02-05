32f43c515df14de44e278297ea7ef198ac38ce6d
amaan <amaan@AMAAN>
2024-02-05 14:14:36 +0530

    Add project files.

---
+++ Car Traders/Screens/customer/Login.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Car_Traders.Screens.customer
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.eyePictureBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.closeBtn = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.LoginBtn1 = new Guna.UI2.WinForms.Guna2Button();
+            this.Logo = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.PasstxtBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.Emailtxtbox = new Guna.UI2.WinForms.Guna2TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.eyePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eyePictureBox
+            // 
+            this.eyePictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.eyePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("eyePictureBox.Image")));
+            this.eyePictureBox.ImageRotate = 0F;
+            this.eyePictureBox.Location = new System.Drawing.Point(519, 293);
+            this.eyePictureBox.Name = "eyePictureBox";
+            this.eyePictureBox.Size = new System.Drawing.Size(34, 19);
+            this.eyePictureBox.TabIndex = 17;
+            this.eyePictureBox.TabStop = false;
+            this.eyePictureBox.UseTransparentBackground = true;
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(100, 100);
+            this.guna2ImageButton1.Image = global::Car_Traders.Properties.Resources.logo;
+            this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(100, 100);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(346, 49);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(110, 110);
+            this.guna2ImageButton1.Size = new System.Drawing.Size(127, 122);
+            this.guna2ImageButton1.TabIndex = 15;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.closeBtn.HoverState.ImageSize = new System.Drawing.Size(43, 43);
+            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
+            this.closeBtn.ImageOffset = new System.Drawing.Point(0, 0);
+            this.closeBtn.ImageRotate = 0F;
+            this.closeBtn.ImageSize = new System.Drawing.Size(40, 40);
+            this.closeBtn.IndicateFocus = true;
+            this.closeBtn.Location = new System.Drawing.Point(791, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.PressedState.ImageSize = new System.Drawing.Size(43, 43);
+            this.closeBtn.Size = new System.Drawing.Size(35, 37);
+            this.closeBtn.TabIndex = 14;
+            // 
+            // LoginBtn1
+            // 
+            this.LoginBtn1.Animated = true;
+            this.LoginBtn1.BackColor = System.Drawing.Color.Transparent;
+            this.LoginBtn1.BorderColor = System.Drawing.Color.Transparent;
+            this.LoginBtn1.BorderRadius = 10;
+            this.LoginBtn1.CustomBorderThickness = new System.Windows.Forms.Padding(5);
+            this.LoginBtn1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.LoginBtn1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.LoginBtn1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.LoginBtn1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.LoginBtn1.FillColor = System.Drawing.Color.RoyalBlue;
+            this.LoginBtn1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn1.ForeColor = System.Drawing.Color.White;
+            this.LoginBtn1.Location = new System.Drawing.Point(359, 357);
+            this.LoginBtn1.Name = "LoginBtn1";
+            this.LoginBtn1.Size = new System.Drawing.Size(114, 46);
+            this.LoginBtn1.TabIndex = 13;
+            this.LoginBtn1.Text = "LOGIN";
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.FillColor = System.Drawing.Color.Transparent;
+            this.Logo.ImageRotate = 0F;
+            this.Logo.Location = new System.Drawing.Point(-25, 49);
+            this.Logo.Name = "Logo";
+            this.Logo.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.Logo.Size = new System.Drawing.Size(86, 87);
+            this.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Logo.TabIndex = 12;
+            this.Logo.TabStop = false;
+            this.Logo.UseTransparentBackground = true;
+            // 
+            // PasstxtBox
+            // 
+            this.PasstxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PasstxtBox.DefaultText = "";
+            this.PasstxtBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.PasstxtBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.PasstxtBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.PasstxtBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.PasstxtBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.PasstxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasstxtBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.PasstxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.PasstxtBox.IconLeft = ((System.Drawing.Image)(resources.GetObject("PasstxtBox.IconLeft")));
+            this.PasstxtBox.IconLeftSize = new System.Drawing.Size(15, 15);
+            this.PasstxtBox.IconRightSize = new System.Drawing.Size(17, 17);
+            this.PasstxtBox.Location = new System.Drawing.Point(269, 283);
+            this.PasstxtBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.PasstxtBox.Name = "PasstxtBox";
+            this.PasstxtBox.PasswordChar = '\0';
+            this.PasstxtBox.PlaceholderText = "Password";
+            this.PasstxtBox.SelectedText = "";
+            this.PasstxtBox.ShadowDecoration.BorderRadius = 15;
+            this.PasstxtBox.Size = new System.Drawing.Size(281, 36);
+            this.PasstxtBox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.PasstxtBox.TabIndex = 11;
+            // 
+            // Emailtxtbox
+            // 
+            this.Emailtxtbox.AutoRoundedCorners = true;
+            this.Emailtxtbox.BorderRadius = 17;
+            this.Emailtxtbox.BorderThickness = 0;
+            this.Emailtxtbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Emailtxtbox.DefaultText = "";
+            this.Emailtxtbox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.Emailtxtbox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.Emailtxtbox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Emailtxtbox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.Emailtxtbox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Emailtxtbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Emailtxtbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Emailtxtbox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.Emailtxtbox.IconLeft = ((System.Drawing.Image)(resources.GetObject("Emailtxtbox.IconLeft")));
+            this.Emailtxtbox.IconLeftSize = new System.Drawing.Size(15, 15);
+            this.Emailtxtbox.Location = new System.Drawing.Point(269, 220);
+            this.Emailtxtbox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Emailtxtbox.Name = "Emailtxtbox";
+            this.Emailtxtbox.PasswordChar = '\0';
+            this.Emailtxtbox.PlaceholderText = "Email";
+            this.Emailtxtbox.SelectedText = "";
+            this.Emailtxtbox.ShadowDecoration.BorderRadius = 15;
+            this.Emailtxtbox.Size = new System.Drawing.Size(281, 36);
+            this.Emailtxtbox.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.Emailtxtbox.TabIndex = 10;
+            this.Emailtxtbox.TextChanged += new System.EventHandler(this.Emailtxtbox_TextChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Car_Traders.Properties.Resources.BG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(826, 477);
+            this.Controls.Add(this.eyePictureBox);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.LoginBtn1);
+            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.PasstxtBox);
+            this.Controls.Add(this.Emailtxtbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.eyePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox eyePictureBox;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2ImageButton closeBtn;
+        private Guna.UI2.WinForms.Guna2Button LoginBtn1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox Logo;
+        private Guna.UI2.WinForms.Guna2TextBox PasstxtBox;
+        private Guna.UI2.WinForms.Guna2TextBox Emailtxtbox;
+    }
+}
